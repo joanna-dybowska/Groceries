34a8948089af9fc598320b2cbb971337f28f2c4a
Joanna Dybowska <joanna.m.dybowska@gmail.com>
2026-04-06 17:41:16 +0200

    Editing gitignore and example pages in main

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ Optionally, the program asks user for corrections on recipes titles.
 The program asks user for an input on number of servings for each recipe.
 '''
 # input file path
-pdf_path = "2000-kcal-DIY-WYSOKOBIALKOWY.pdf"
+pdf_path = "results/2000-kcal-DIY-WYSOKOBIALKOWY.pdf"
 
 # results file paths
-txt_path = "recipes.txt"
-xlsx_path = "groceries.xlsx"
+txt_path = "results/4/recipes.txt"
+xlsx_path = "results/4/groceries.xlsx"
 
 # STEP 1 - create TXT file with recipes
 pages_numbers = [86, 93, 128] # [73, 79, 87, 106, 108, 109, 136, 140, 147]
-recipe_pdf = RecipePDF(pdf_path, txt_path, [86, 93])
+recipe_pdf = RecipePDF(pdf_path, txt_path, pages_numbers)
 recipe_pdf.correct_titles_user_input()
 recipe_pdf.save_recipes_txt_file()
 
