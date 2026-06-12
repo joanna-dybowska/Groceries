1a8418738a3c652590568e9edf75857d4f204129
Joanna Dybowska <joanna.m.dybowska@gmail.com>
2026-06-12 16:49:15 +0200

    Parametrizing file paths

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from modules.recipe_pdf_class import RecipePDF
 from modules.groceries_class import Groceries
+from datetime import datetime as dt
+import os
 
-'''
-This code reads recipes from PDF file (based on "Wysokobiałkowy wegański jadłospis DIY" by Irena Owsiak) to a TXT file.
-Then from recipes from TXT file an XLSX file is created with ingredients to buy.
-Optionally, the program asks user for corrections on recipes titles.
-The program asks user for an input on number of servings for each recipe.
-'''
 # input file path
 pdf_path = "results/2000-kcal-DIY-WYSOKOBIALKOWY.pdf"
 
 # results file paths
-txt_path = "results/4/recipes.txt"
-xlsx_path = "results/4/groceries.xlsx"
+folder_today = dt.today().strftime("%Y-%m-%d")
+folder_results = f"results/{folder_today}"
+if not os.path.exists(folder_results):
+    os.makedirs(folder_results)
+
+txt_path = f"{folder_results}/recipes.txt"
+xlsx_path = f"{folder_results}/groceries.xlsx"
 
 # STEP 1 - create TXT file with recipes
-pages_numbers = [86, 93, 128] # [73, 79, 87, 106, 108, 109, 136, 140, 147]
+pages_numbers = [73, 79, 106, 137, 140] # [73, 79, 87, 106, 108, 109, 136, 137, 140]
 recipe_pdf = RecipePDF(pdf_path, txt_path, pages_numbers)
 recipe_pdf.correct_titles_user_input()
 recipe_pdf.save_recipes_txt_file()
